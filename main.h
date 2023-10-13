3e6fc571a61a0f99a6e1d465debc11adb96e38f2
FavourAbraham <fa4abraham18@gmail.com>
2023-10-13 14:10:50 -0700

    Done for prinf 1st task

--- main.h
+++ main.h
@@ -46,11 +46,11 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 /****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
-int print_char(va_list types, char buffer[],
+int myownprint_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
+int myownprint_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[],
+int myownprint_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 /* Functions to print numbers */
